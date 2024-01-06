7c567b97b0a7928c63d5f41ec41120e6637db848
niole <niolenelson@gmail.com>
2024-01-06 14:52:01 -0800

    Figured out how to integrate oauth

---
+++ README.md
@@ -0,0 +1,30 @@
+# Install
+
+## dependencies
+- expo
+- npm
+
+```sh
+npm install -g eas-cli
+npm i
+```
+
+
+# Dev Mode
+This app uses expo development builds because it uses the google oath API for react native x android
+
+## create new dev build
+why? bc you installed new react-native code and you want to use it in dev mode.
+
+```sh
+npm run dev-build
+
+# then scan the QR code with your mobile device's camera
+# don't use an emulator
+# follow the instructions in the UI
+```
+
+## run app
+```sh
+npm run start-dev-client
+```
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from 'react';
 import { Threads } from './Threads';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
+import 'react-native-gesture-handler';
+
+// TODO this may start with a login screen
 export default function App() {
+  const [isSigninInProgress, setSigninWip] = useState<boolean>(false);
+  const [userInfo, setUserInfo] = useState<any>();
+  useEffect(() => {
+    GoogleSignin.configure({
+        scopes: ['https://www.googleapis.com/auth/gmail.readonly'],
+        forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+    });
+  });
+
+  async function signIn() {
+    try {
+      await GoogleSignin.hasPlayServices();
+      setUserInfo(await GoogleSignin.signIn());
+    } catch (error: any) {
+      console.error(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('// user cancelled the login flow');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('// operation (e.g. sign in) is in progress already');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('// play services not available or outdated');
+      } else {
+        console.log('// some other error happened');
+      }
+    }
+  }
+
+  console.log(userInfo);
   return (
+    <>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+        disabled={isSigninInProgress}
+      />
       <Threads />
+    </>
   );
 }
